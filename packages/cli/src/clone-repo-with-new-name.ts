e2cd6238135edac3d3169c315bd10a1d2745718a
gfffrtt <geogueserporque@gmail.com>
2025-01-31 22:37:47 -0300

    feat: cli finished

--- packages/cli/src/clone-repo-with-new-name.ts
+++ packages/cli/src/clone-repo-with-new-name.ts
@@ -4,6 +4,8 @@ export const cloneRepoWithNewName = async (
   repoName: string,
   pathToTemplate: string
 ) => {
-  await $`git clone --depth=1 --filter=blob:none --no-checkout --sparse https://github.com/gfffrtt/alaska-web.git ${repoName}`;
-  await $`cd ${repoName} && git sparse-checkout set ${pathToTemplate} && git checkout`;
+  await $`git clone --depth=1 https://github.com/gfffrtt/alaska-web.git`;
+  await $`mkdir ${repoName}`;
+  await $`cp -r alaska-web/${pathToTemplate}/* ${repoName}`;
+  await $`rm -rf alaska-web`;
 };
