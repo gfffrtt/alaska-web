42c5648e1308f0e0de61e510a8b1551a1af460ce
gfffrtt <geogueserporque@gmail.com>
2025-01-31 22:07:03 -0300

    repo

--- packages/alaska/http.go
+++
@@ -1,9 +0,0 @@
-package alaska
-
-import "net/http"
-
-type HttpServer interface {
-	Get(string, func(http.ResponseWriter, *http.Request))
-	Post(string, func(http.ResponseWriter, *http.Request))
-	Static()
-}
