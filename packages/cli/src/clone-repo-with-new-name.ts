b50c1a9fc1bb4bcd824c2cf847ca946b471e35ad
gfffrtt <geogueserporque@gmail.com>
2025-01-31 22:43:21 -0300

    feat: finished alaska cli

--- packages/cli/src/clone-repo-with-new-name.ts
+++ packages/cli/src/clone-repo-with-new-name.ts
@@ -1,11 +1,34 @@
+import { tasks } from "@clack/prompts";
 import { $ } from "bun";
 
 export const cloneRepoWithNewName = async (
   repoName: string,
   pathToTemplate: string
 ) => {
-  await $`git clone --depth=1 https://github.com/gfffrtt/alaska-web.git`;
-  await $`mkdir ${repoName}`;
-  await $`cp -r alaska-web/${pathToTemplate}/* ${repoName}`;
-  await $`rm -rf alaska-web`;
+  await tasks([
+    {
+      title: "Cloning template",
+      task: async () => {
+        await $`git clone --depth=1 https://github.com/gfffrtt/alaska-web.git`;
+      },
+    },
+    {
+      title: "Creating new repository",
+      task: async () => {
+        await $`mkdir ${repoName}`;
+      },
+    },
+    {
+      title: "Copying template",
+      task: async () => {
+        await $`cp -r alaska-web/${pathToTemplate}/* ${repoName}`;
+      },
+    },
+    {
+      title: "Cleaning up",
+      task: async () => {
+        await $`rm -rf alaska-web`;
+      },
+    },
+  ]);
 };
