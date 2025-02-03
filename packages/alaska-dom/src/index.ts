f6a3e15848c8dcacab51d682243c580b63a2e0ba
gfffrtt <geogueserporque@gmail.com>
2025-02-03 20:54:50 -0300

    feat: add get props

--- packages/alaska-dom/src/index.ts
+++ packages/alaska-dom/src/index.ts
@@ -3,3 +3,6 @@ import { router } from "./router/router";
 export const server = () => {
   router();
 };
+
+export const getProps = <T>(id: string): T =>
+  JSON.parse(document.getElementById(id)!.innerText);
